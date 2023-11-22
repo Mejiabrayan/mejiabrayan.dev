758a0fbd3248152308299bddcaae54c387112097
Your Name <brayanmejiacuenca@Brayans-MacBook-Air.local>
2023-11-21 22:58:12 -0800

    initalized Contentful client

---
+++ src/lib/contentful.ts
@@ -0,0 +1,25 @@
+import contentful from "contentful";
+import type { EntryFieldTypes } from "contentful";
+
+export interface BlogPost {
+  contentTypeId: "blogPost",
+  fields: {
+    title: EntryFieldTypes.Text
+    content: EntryFieldTypes.RichText,
+    date: EntryFieldTypes.Date,
+    description: EntryFieldTypes.Text,
+    slug: EntryFieldTypes.Text
+  }
+}
+
+export interface fetchAllBlogPosts {
+  contentTypeId: "project",
+}
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
