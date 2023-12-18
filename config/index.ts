287e8cabc7d637675a8ea393ebe44dbae8941d94
Your Name <brayanmejiacuenca@Brayans-MacBook-Air.local>
2023-12-18 11:10:44 -0800

    Add config file with social and navigation links

---
+++ config/index.ts
@@ -0,0 +1,19 @@
+import type { SocialLinks, NavLinks } from 'types';
+
+export const socialLinks: SocialLinks = [
+  {
+    linkedIn: 'https://www.linkedin.com/in/brayan-mejia/',
+    github: 'https://github.com/Mejiabrayan',
+    twitter: 'https://twitter.com/cyperpunkdev',
+  },
+];
+
+export const navLinks: NavLinks = [
+  {
+    Home: { label: 'Home', href: '/' },
+    About: { label: 'About', href: '/about' },
+    Projects: { label: 'Projects', href: '/projects' },
+    Blog: { label: 'Blog', href: '/blog' },
+    Contact: { label: 'Contact', href: '/contact' },
+  },
+];
