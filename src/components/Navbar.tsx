2be095a77cecd7ffb1870668d7be55c0b52a896b
Your Name <brayanmejiacuenca@Brayans-MacBook-Air.local>
2024-01-25 12:37:52 -0800

    Update Navbar icon and blog page design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,25 @@
 import { FloatingNav } from './floating-navbar';
-import { IconHeart, IconHome, IconMessage, IconUser } from '@tabler/icons-react';
+import {
+  IconHeart,
+  IconHome,
+  IconMessage,
+  IconUser,
+} from '@tabler/icons-react';
 export function Navbar() {
   const navItems = [
     {
       name: 'Work',
       link: '/#work',
-      icon: <IconHeart className='h-4 w-4 text-neutral-500 dark:text-accents-8' />,
+      icon: (
+        <IconHeart className='h-4 w-4 text-neutral-500 dark:text-accents-8' />
+      ),
     },
     {
-      name: 'About',
-      link: '/#about',
-      icon: <IconUser className='h-4 w-4 text-neutral-500 dark:text-accents-8' />,
+      name: 'Blogs',
+      link: '/blogs',
+      icon: (
+        <IconUser className='h-4 w-4 text-neutral-500 dark:text-accents-8' />
+      ),
     },
     {
       name: 'Contact',
