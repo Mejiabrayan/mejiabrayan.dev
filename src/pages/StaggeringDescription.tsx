d84a3c12293bcfe5c12e96e4bc7640cacc1664ab
Your Name <brayanmejiacuenca@Brayans-MacBook-Air.local>
2024-03-21 10:39:23 -0700

    added animation to description

---
+++ src/pages/StaggeringDescription.tsx
@@ -0,0 +1,49 @@
+import { motion } from 'framer-motion';
+import { CornerRightDown } from 'lucide-react';
+
+const StaggeringDescription = () => {
+  const fadeAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      y: 0,
+
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  };
+
+  const paragraphContent = [
+    { text: 'Continuous learning ', isAccent: true },
+    {
+      text: "is a core value of mine. I'm always looking to expand my skillset and learn new technologies. Here are some of the projects I've worked on",
+      isAccent: false,
+    },
+  ];
+
+  return (
+    <motion.div className='absolute text-lg md:text-xl lg:text-2xl text-accents-6'>
+      {paragraphContent.map((segment, index) => (
+        <motion.p
+          key={index}
+          className={segment.isAccent ? 'text-accents-7' : ''}
+          variants={fadeAnimationVariants}
+          initial='initial'
+          whileInView='animate'
+          viewport={{
+            once: true,
+          }}
+          custom={index}
+        >
+          {segment.text} <CornerRightDown className='inline-block w-6 h-6' />
+        </motion.p>
+      ))}
+    </motion.div>
+  );
+};
+
+export default StaggeringDescription;
