7b5a1798ef2e156d5b0bbd29912d73e79da598aa
Brayan Mejia Cuenca <mejiabrayan@protonmail.com>
2024-06-10 21:29:15 -0700

    removed old components & converted blogCard react component into an astro component

---
+++ types/index.d.ts
@@ -0,0 +1,37 @@
+
+export type SocialLinks = {
+    linkedIn: string;
+    github: string;
+    twitter: string;
+  }[];
+  
+  export type NavItems = {
+    name: string;
+    link: string;
+    icon?: Element;
+  }[];
+  
+  export interface ProjectItem {
+    title: string;
+    image: any | React.ReactNode;
+    description: string;
+    slug: string;
+    caseStudy?: string;
+    date: string;
+    className?: string;
+  }
+  
+  export interface ProjectCardsProps {
+    projects: ProjectItem[];
+  }
+  
+  export interface BlogItems {
+    title: string;
+    date: string;
+    description: string;
+    slug: string;
+  }
+  
+  export interface BlogCardsProps {
+    posts: BlogItems[];
+  }
